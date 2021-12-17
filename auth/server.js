e0c1ba7f69344d64d6eb22cd1b7e949422d63f30
Brandon Lodowica <brandonlodowica@hotmail.com>
2021-12-17 02:52:52 +0100

    Revert "test changes"
    This reverts commit 1ca9a53743ae833fcb537e154b8222f82cf203a7.

--- auth/server.js
+++ auth/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded());
 
 app.post("/auth", function (req, res) {
     /* This server is only available to nginx */
